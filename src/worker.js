d40546ec84d518a441dc127593d13d292377be59
刘传 <769601722@qq.com>
2024-05-23 14:07:19 +0800

    update

--- src/worker.js
+++ src/worker.js
@@ -11,8 +11,43 @@ import { Router } from 'itty-router';
 import { Render } from './face/api';
 
 const router = Router();
+const template = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>潦草头像</title>
+</head>
+<style>
+    img {
+        width: 100%;
+        max-width: 500px;
+    }
+</style>
+<body>
+    <img id="avatar" src="/api/avatar" alt="头像">
+    <button onclick="switchAvatar()">切换</button>
+</body>
+<script>
+    function switchAvatar() {
+        var avatar = document.querySelector("#avatar");
+        var url = new URL(avatar.src);
+        url.searchParams.set('t', new Date().getTime());
+        avatar.src = url.href;
+    }
+</script>
+</html>`;
 
-router.get('/', () => new Response('访问 /api/avatar 获取头像'));
+router.get(
+	'/',
+	() =>
+		new Response(template, {
+			headers: {
+				'Content-Type': 'text/html',
+			},
+		})
+);
 
 router.get(
 	'/api/avatar',
